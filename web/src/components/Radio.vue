7a8c2b6139f4c4483437fdab3cb8dd20f76f281e
Niunisas <55489629+niunisas@users.noreply.github.com>
2022-09-05 14:04:17 +0300

    feat: add radio UI

---
+++ web/src/components/Radio.vue
@@ -0,0 +1,95 @@
+<template>
+	<div
+		class="absolute bottom-0 right-2 transform transition duration-150 ease-linear"
+		:class="{
+			'translate-y-[15vh]': app.apps.radio,
+			'translate-y-[1000px]': !app.apps.radio,
+		}"
+	>
+		<img :src="radio.getThemeFrame" class="h-[70vh] drop-shadow-2xl" />
+		<div class="absolute top-[35.5%] left-[22.4%] bg-base-300 w-[55.2%] h-[30.35%] rounded-[6.2%] overflow-hidden" ref="tabsList">
+			<transition-group name="fade" mode="out-in">
+				<component v-for="(component, index) in tabList" :is="component" :key="index" v-show="index === radio.currentTab" />
+			</transition-group>
+		</div>
+	</div>
+</template>
+<script setup lang="ts">
+import { inject, type Component } from 'vue'
+import { useKeypress } from 'vue3-keypress'
+
+import { resource } from '@/utils'
+import { useAxios } from '@vueuse/integrations/useAxios'
+
+import Connection from './Radio/Connection.vue'
+import Change from './Radio/Change.vue'
+import List from './Radio/List.vue'
+
+import { useApp } from '@/stores/app'
+import { useRadio } from '@/stores/radio'
+
+const emitter: any = inject('emitter')
+const app = useApp()
+const radio = useRadio()
+
+const tabList: { [key: string]: Component } = {
+	connection: Connection,
+	change: Change,
+	list: List,
+}
+
+function close(send?: boolean) {
+	app.apps.radio = false
+
+	if (send) {
+		useAxios(`https://${resource()}/closeRadio`, {
+			method: 'POST',
+			data: false,
+		})
+	}
+}
+
+useKeypress({
+	keyEvent: 'keyup',
+	keyBinds: [
+		{
+			keyCode: 'esc',
+			success: () => {
+				if (app.apps.radio) {
+					close(true)
+				}
+			},
+		},
+	],
+})
+
+emitter.on('radio:open', (event: any) => {
+	app.apps.radio = true
+
+	useAxios(`https://${resource()}/enableFocus`, {
+		method: 'POST',
+		data: {
+			isFocused: true,
+			cursorToggle: true,
+			keepInput: event.keepInput,
+		},
+	})
+})
+
+emitter.on('radio:close', () => {
+	close()
+})
+
+emitter.on('radio:changeTheme', (event: any) => {
+	radio.theme = event.theme
+})
+
+emitter.on('radio:updateValues', (event: any) => {
+	if (Object.keys(event).length <= 0) return
+
+	for (const [key, value] of Object.entries(event)) {
+		//@ts-ignore
+		radio[key] = value
+	}
+})
+</script>
---
+++ web/src/components/Radio/Change.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="flex flex-col overflow-hidden h-full">
+		<div class="flex items-center w-full h-[12%] bg-primary/50 select-none">
+			<div class="flex flex-1 items-baseline gap-1 pl-2">
+				<i class="fa-solid fa-signal text-[calc(8px_+_0.390625vh)] 2xl:text-[calc(11px_+_0.390625vh)] leading-none" v-if="radio.current"></i>
+				<i class="fa-solid fa-xmark text-[calc(8px_+_0.390625vh)] 2xl:text-[calc(11px_+_0.390625vh)] leading-none" v-if="!radio.current"></i>
+				<span class="text-[calc(8px_+_0.390625vh)] 2xl:text-[calc(11px_+_0.390625vh)] font-semibold leading-none" v-if="radio.current">{{ radio.current }}</span>
+			</div>
+			<div class="w-0.5 h-[70%] bg-secondary/40 rounded-full"></div>
+			<div class="flex flex-1 items-center justify-end gap-1 pr-2">
+				<span class="text-[calc(8px_+_0.390625vh)] 2xl:text-[calc(11px_+_0.390625vh)] font-semibold">{{ radio.volume }}</span>
+				<i class="fa-solid fa-volume-high text-[calc(8px_+_0.390625vh)] 2xl:text-[calc(11px_+_0.390625vh)]"></i>
+			</div>
+		</div>
+		<div class="flex flex-col flex-1 overflow-auto scl p-4 gap-4">
+			<div class="flex bg-primary/50 w-full rounded-md py-1 px-2 items-center hover:text-primary-content hover:bg-primary gap-3 select-none" @click="radio.setTab('connection')">
+				<i class="fa-solid fa-arrow-left text-[calc(12px_+_0.390625vh)]"></i>
+				<span class="text-[calc(12px_+_0.390625vh)]">{{ locale.t('back_radio') }}</span>
+			</div>
+			<div class="flex flex-col justify-evenly bg-primary/50 w-full rounded-md p-3 gap-1 flex-1">
+				<input type="text" :placeholder="locale.t('radio_call_sign_placeholder')" class="input input-bordered w-full max-w-xs" v-model="radio.change.sign" />
+				<input type="text" :placeholder="locale.t('radio_name_placeholder')" class="input input-bordered w-full max-w-xs" v-model="radio.change.name" />
+			</div>
+		</div>
+		<div class="flex items-center w-full h-[12%] bg-primary/50 select-none">
+			<div class="flex flex-1 items-center justify-center gap-1 pl-2 hover:bg-primary hover:text-primary-content h-full" @click="radio.updateName()">
+				<span class="text-[75%] font-semibold">{{ locale.t('change_radio') }}</span>
+			</div>
+		</div>
+	</div>
+</template>
+<script setup lang="ts">
+import { useRadio } from '@/stores/radio'
+import { useLocale } from '@/stores/locale'
+
+const radio = useRadio()
+const locale = useLocale()
+</script>
